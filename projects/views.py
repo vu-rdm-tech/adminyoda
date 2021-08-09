6427059cf213a87ffbe3d887d8e716cc488366c9
peer35 <p.j.m.vos@vu.nl>
2021-08-09 10:37:11 +0200

    improve stats collation

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,10 @@
 from django.http import Http404, JsonResponse
 from django.shortcuts import render
-from .models import Project, MiscStats, VaultDataset, ResearchFolder, Department, VaultFolder, VaultStats, ResearchStats, Person, Datamanager
+from .models import Project, MiscStats, VaultDataset, ResearchFolder, Department, VaultFolder, VaultStats, \
+    ResearchStats, Person, Datamanager
 from datetime import datetime
 
-GB=1024*1024*1024
+GB = 1024 * 1024 * 1024
 start_month = 6
 start_year = 2021
 today = datetime.now()
@@ -13,8 +14,10 @@ COLORSET = ['rgba(141,211,199)', 'rgba(255,255,179)', 'rgba(190,186,218)', 'rgba
             'rgba(253,180,98)', 'rgba(179,222,105)', 'rgba(252,205,229)', 'rgba(217,217,217)', 'rgba(188,128,189)',
             'rgba(204,235,197)', 'rgba(255,237,111)']
 
+
 class CustomObject():
     pass
+
 
 def convert_bytes(num):
     """
@@ -25,35 +28,38 @@ def convert_bytes(num):
             return "%3.1f %s" % (num, x)
         num /= 1024.0
 
+
 # Create your views here.
 def projects_index(request):
     pr = Project.objects.all().order_by('title')
     data = []
     for p in pr:
         d = CustomObject()
-        d.id=p.id
-        d.title=p.title
-        d.department=p.department.name
-        d.faculty=p.department.faculty
-        d.requested_size=convert_bytes(p.requested_size * GB)
-        d.limit=convert_bytes(p.storage_limit * GB)
-        d = _get_rf(p,d)
+        d.id = p.id
+        d.title = p.title
+        d.department = p.department.name
+        d.faculty = p.department.faculty
+        d.requested_size = convert_bytes(p.requested_size * GB)
+        d.limit = convert_bytes(p.storage_limit * GB)
+        d = _get_rf(p, d)
         data.append(d)
     d = CustomObject()
-    d = _get_rf(None, d) # unconnected research folders
+    d = _get_rf(None, d)  # unconnected research folders
     d.title = '-'
     d.department = '-'
     d.faculty = '-'
-    if d.num_groups>0:
+    if d.num_groups > 0:
         data.append(d)
     context = {
         'projects': data
     }
     return render(request, 'projects/index.html', context=context)
 
+
 def projects_storage(request):
-    context={}
+    context = {}
     return render(request, 'projects/storage.html', context=context)
+
 
 def _get_rf(p, d):
     if p is None:
@@ -75,39 +81,40 @@ def _get_rf(p, d):
             pubcnt = VaultDataset.objects.filter(status='PUBLISHED', vault_folder=vf).count()
             d.num_dataset = d.num_dataset + cnt
             d.num_published = d.num_published + pubcnt
-    d.size_warning=False
+    d.size_warning = False
     if p is not None:
         if size > (p.storage_limit * GB):
-            d.size_warning=True
+            d.size_warning = True
     d.size = convert_bytes(size)
 
     return d
+
 
 def project_detail(request, project_id):
     try:
         project = Project.objects.get(pk=project_id)
         rf = ResearchFolder.objects.filter(project=project)
-        data=CustomObject()
-        data.project=project
-        data.research_folders=[]
+        data = CustomObject()
+        data.project = project
+        data.research_folders = []
 
-        total_research_size=0
-        total_vault_size=0
+        total_research_size = 0
+        total_vault_size = 0
         for f in rf:
             if f.deleted is not None:
-                deleted=True
+                deleted = True
             else:
-                deleted=False
+                deleted = False
             rf_data = CustomObject()
-            rf_data.datamanagers=[]
-            rf_data.category=f.category
+            rf_data.datamanagers = []
+            rf_data.category = f.category
             dms = Datamanager.objects.filter(category=f.category)
-            rf_data.datamanagers_object=len(dms)
+            rf_data.datamanagers_object = len(dms)
             for dm in dms:
                 rf_data.datamanagers.append(f'{dm.user.firstname} {dm.user.lastname} ({dm.user.vunetid})')
-            rf_data.research_name=f.yoda_name
+            rf_data.research_name = f.yoda_name
             if deleted:
-                rf_data.research_name=f'{rf_data.research_name} [deleted]'
+                rf_data.research_name = f'{rf_data.research_name} [deleted]'
                 s = 0
             else:
                 s = ResearchStats.objects.filter(research_folder=f).latest('collected').size
@@ -121,74 +128,86 @@ def project_detail(request, project_id):
             else:
                 s = VaultStats.objects.filter(vault_folder=vf).latest('collected').size
                 datasets = VaultDataset.objects.filter(vault_folder=vf)
-                rf_data.datasets=[]
+                rf_data.datasets = []
                 for dataset in datasets:
-                    ds_data=CustomObject()
-                    ds_data.status=dataset.status
-                    ds_data.name=dataset.yoda_name
+                    ds_data = CustomObject()
+                    ds_data.status = dataset.status
+                    ds_data.name = dataset.yoda_name
                     rf_data.datasets.append(ds_data)
             rf_data.vault_size = convert_bytes(s)
             total_vault_size += s
             data.research_folders.append(rf_data)
-        data.research_size=convert_bytes(total_research_size)
-        data.vault_size=convert_bytes(total_vault_size)
+        data.research_size = convert_bytes(total_research_size)
+        data.vault_size = convert_bytes(total_vault_size)
     except Project.DoesNotExist:
         raise Http404("Project does not exist")
 
     return render(request, 'projects/details.html', context={'data': data})
 
+
 def _monthly_researchstats(research_folder):
-    stats = []
-    last_size=0
+    stats = {}
+    last_size = 0
     for year in range(start_year, end_year + 1):
         for month in range(1, 12):
-            s = ResearchStats.objects.filter(research_folder=research_folder, collected__year=year, collected__month=month).order_by('collected').last()
+            s = ResearchStats.objects.filter(research_folder=research_folder, collected__year=year,
+                                             collected__month=month).order_by('collected').last()
             if s is not None:
-                s.label = f'{year}-{month}'
-                s.delta=s.size-last_size
-                last_size=s.size
-                stats.append(s)
-    if len(stats)>1:
-       stats[0].delta=0
+                size = s.size
+                label = f'{year}-{month}'
+                stats[label] = {}
+                delta = size - last_size
+                last_size = size
+                stats[label]['size'] = size
+                stats[label]['delta'] = delta
     return stats
 
+
 def _monthly_vaultstats(vault_folder):
-    stats = []
-    last_size=0
+    stats = {}
+    last_size = 0
     for year in range(start_year, end_year + 1):
         for month in range(1, 13):
-            s = VaultStats.objects.filter(vault_folder=vault_folder, collected__year=year, collected__month=month).order_by('collected').last()
+            s = VaultStats.objects.filter(vault_folder=vault_folder, collected__year=year,
+                                          collected__month=month).order_by('collected').last()
             if s is not None:
-                s.label = f'{year}-{month}'
-                s.delta=s.size-last_size
-                last_size=s.size
-                #s.label = s.collected
-                stats.append(s)
-    if len(stats)>1:
-        stats[0].delta=0
+                size = s.size
+                label = f'{year}-{month}'
+                stats[label] = {}
+                delta = size - last_size
+                last_size = size
+                stats[label]['size'] = size
+                stats[label]['delta'] = delta
     return stats
+
 
 def _research_stats(project_id):
     project = Project.objects.get(pk=project_id)
-    rf = ResearchFolder.objects.filter(project=project) # deleted folders should automatically disappear from the stats
-    research_stats=[]
-    vault_stats=[]
+    rf = ResearchFolder.objects.filter(project=project)  # deleted folders should automatically disappear from the stats
+    research_stats = {}
+    vault_stats = {}
     for f in rf:
         rstats = _monthly_researchstats(f)
         vf = VaultFolder.objects.get(research_folder=f)
         vstats = _monthly_vaultstats(vf)
-        i=0
-        for rs in rstats:
-            if len(research_stats) >= i+1:
-                research_stats[i].size += rs.size
-                vault_stats[i].size += vstats[i].size
-                research_stats[i].delta += rs.delta
-                vault_stats[i].delta += vstats[i].delta
-            else:
-                research_stats.append(rs)
-                vault_stats.append(vstats[i])
-            i+=1
+        i = 0
+        # Now merge these on label
+        for label in rstats:
+            if label not in research_stats:
+                research_stats[label] = {}
+                research_stats[label]['size'] = 0
+                research_stats[label]['delta'] = 0
+            research_stats[label]['size'] += rstats[label]['size']
+            research_stats[label]['delta'] += rstats[label]['delta']
+        for label in vstats:
+            if label not in vault_stats:
+                vault_stats[label] = {}
+                vault_stats[label]['size'] = 0
+                vault_stats[label]['delta'] = 0
+            vault_stats[label]['size'] += vstats[label]['size']
+            vault_stats[label]['delta'] += vstats[label]['delta']
     return research_stats, vault_stats
+
 
 def project_size_chart_json(request, project_id):
     labels = []
@@ -196,12 +215,12 @@ def project_size_chart_json(request, project_id):
     vault = []
     div = (1024 * 1024 * 1024)
     research_stats, vault_stats = _research_stats(project_id)
-    i=0
-    for rs in research_stats:
-        labels.append(rs.label)
-        research.append(round(rs.size / div, 2))
-        vault.append(round(vault_stats[i].size / div, 2))
-        i+=1
+    i = 0
+    for label in research_stats:
+        labels.append(label)
+        research.append(round(research_stats[label]['size'] / div, 2))
+        vault.append(round(vault_stats[label]['size'] / div, 2))
+        i += 1
     datasets = [
         {
             'label': 'Research',
@@ -220,18 +239,19 @@ def project_size_chart_json(request, project_id):
     ]
     return JsonResponse(data={'labels': labels, 'datasets': datasets})
 
+
 def project_delta_chart_json(request, project_id):
     labels = []
     research = []
     vault = []
     div = (1024 * 1024 * 1024)
     research_stats, vault_stats = _research_stats(project_id)
-    i=0
-    for rs in research_stats:
-        labels.append(rs.label)
-        research.append(round(rs.delta / div, 2))
-        vault.append(round(vault_stats[i].delta / div, 2))
-        i+=1
+    i = 0
+    for label in research_stats:
+        labels.append(label)
+        research.append(round(research_stats[label]['delta'] / div, 2))
+        vault.append(round(vault_stats[label]['delta'] / div, 2))
+        i += 1
     datasets = [
         {
             'label': 'Research',
