13c24f8ec96ed72f83e8e8dce0e011525236f917
peer35 <p.j.m.vos@vu.nl>
2022-06-30 17:20:39 +0200

    email report with templates

--- projects/reports.py
+++ projects/reports.py
@@ -1,35 +1,33 @@
 from projects.views import project_detail_data
 from projects.models import Project
+import logging
+import os
+import datetime
+from django.conf import settings
+
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
+logger = logging.getLogger(__name__)
 
 
-def message_text(project_id):
-    details = project_detail_data(project_id)
-    project = Project.objects.get(pk=project_id)
-    text = 'Project details:\r\n' \
-           f'Title: {details.project.title}\r\n' \
-           f'Project owner: {project.owner.firstname} {project.owner.lastname}\r\n' \
-           f'Department: {project.department.faculty} {project.department.institute} {project.department.name}\r\n' \
-           f'Budget code: {project.budget.code}\r\n' \
-           f'Budget owner: {project.budget.vunetid}\r\n' \
-           'Please contact us at rdm@vu.nl if this information is incorrect or needs to be updated\r\n\r\n'
+def send_monthly_owner_reports():
+    projects = Project.objects.all()
+    for project in projects:
+        details = project_detail_data(project.id)
+        project = Project.objects.get(pk=project.id)
+        msg_plain = render_to_string('projects/owner_report.txt', {'project': project, 'details': details, 'sitename': os.environ.get("SITE_NAME")})
+        msg_html = render_to_string('projects/owner_report.html', {'project': project, 'details': details, 'sitename': os.environ.get("SITE_NAME")})
+        try:
+            send_mail(
+                f'{datetime.datetime.now().strftime("%B")} Yoda usage report for "{project.title}"',
+                msg_plain,
+                settings.REPORTS_MAIL_FROM,
+                [settings.REPORTS_MAIL_TEST_TO],
+                html_message=msg_html,
+            )
+            logger.info(f'successfully sent monthly report email to {settings.REPORTS_MAIL_TEST_TO}')
+        except Exception as e:
+            logger.error(f'unable to send monthly report email to {settings.REPORTS_MAIL_TEST_TO}, error: {e}')
 
-    text += 'Folders:\r\n'
-    for r in details.research_folders:
-        text += f'{r.research_name}: {r.research_size}\r\n' \
-                f'{r.vault_name}: {r.vault_size}\r\n'
-        if len(r.datasets) > 0:
-            text += '\tDatasets in this Vault:\r\n'
-        else:
-            text += '\tNo datasets in this Vault'
-        for d in r.datasets:
-            print(d.name)
-            text += f'\t\t{d.name} ({d.status}, retention:{d.retention}): {d.csize}\r\n'
-        text += '\r\n'
-    text += '\r\nProject total:\r\n' \
-            f'Research:\t{details.research_size} = {details.research_blocks} {details.research_block_size} block(s)\r\n' \
-            f'Vault:\t\t{details.vault_size} = {details.vault_blocks} {details.vault_block_size} block(s)\r\n' \
-            f'\r\nMore information can be found on https://adminyoda.labs.vu.nl/projects/{project_id} (on campus or via eduvpn)'
-    return text
-
-
-print(message_text(7))
+send_monthly_owner_reports()
