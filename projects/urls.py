ff335a0ed6885e6df43459e4fdeed84060cbd4f2
peer35 <p.j.m.vos@vu.nl>
2023-02-28 12:28:50 +0100

    improve projects table #22

--- projects/urls.py
+++ projects/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.projects_index, name='projects_index'),
+    path('', views.projects_index_table, name='projects_index'),
     path('storage', views.projects_storage, name='projects_storage'),
     path('<int:project_id>', views.project_detail, name='project_detail'),
     path('project_size_chart_json/<int:project_id>', views.project_size_chart_json, name='project_size_chart_json'),
