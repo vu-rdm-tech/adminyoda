5aae2e703b235ea3d63ef418ae7cad281d648b82
Peter Vos <pjmvos@gmail.com>
2024-02-15 11:06:00 +0100

    add research size breakdown chart

--- projects/urls.py
+++ projects/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('storage', views.projects_storage, name='projects_storage'),
     path('<int:project_id>', views.project_detail, name='project_detail'),
     path('project_size_chart_json/<int:project_id>', views.project_size_chart_json, name='project_size_chart_json'),
+    path('size_breakdown_chart_json', views.size_breakdown_chart_json, name='size_breakdown_chart_json'),
     path('project_research_size_chart_json/<int:project_id>', views.project_research_size_chart_json, name='project_research_size_chart_json'),
     path('project_vault_size_chart_json/<int:project_id>', views.project_vault_size_chart_json, name='project_vault_size_chart_json'),
     path('project_delta_chart_json/<int:project_id>', views.project_delta_chart_json, name='project_delta_chart_json'),
